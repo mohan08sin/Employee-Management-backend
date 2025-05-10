7dbe847543b7e3ae1cc88569543884a56db80917
Mohan <84113216+mohan08sin@users.noreply.github.com>
2025-05-11 03:31:02 +0530

    Update EmployeeManagementApplication.java

--- src/main/java/com/emp/management/Employee/Management/EmployeeManagementApplication.java
+++ src/main/java/com/emp/management/Employee/Management/EmployeeManagementApplication.java
@@ -3,23 +3,16 @@ package com.emp.management.Employee.Management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication
-// public class EmployeeManagementApplication {
-
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(EmployeeManagementApplication.class, args);
-// 	}
-
-// }
-
-
 @SpringBootApplication
-@EnableScheduling
 public class EmployeeManagementApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(YourApp.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
+
 }
+
+
 
 
 
