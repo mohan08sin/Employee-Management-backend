ca2702aa0e19c45bc521b117cda5bba4bc071f85
Mohan <84113216+mohan08sin@users.noreply.github.com>
2025-05-11 03:29:08 +0530

    Update EmployeeManagementApplicationTests.java

--- src/test/java/com/emp/management/Employee/Management/EmployeeManagementApplicationTests.java
+++ src/test/java/com/emp/management/Employee/Management/EmployeeManagementApplicationTests.java
@@ -4,14 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@SpringBootApplication
-@EnableScheduling
 public class EmployeeManagementApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-	
-    public static void main(String[] args) {
-        SpringApplication.run(YourApp.class, args);
-    }
 }
